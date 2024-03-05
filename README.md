e91cf898821a02e6e4d1ca3168dbd32431e1406d
John <jowamumjwesh@gmail.com>
2024-03-05 21:26:47 +0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# calculator
+# Simple Calculator Program in Dart
+
+This is a basic calculator program written in Dart. It takes user input for two numbers and an operator, then performs the corresponding operation.
+
+## Features
+
+- Addition
+- Subtraction
+- Multiplication
+- Division
+- Modulus
+
+## Usage
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/your-username/dart-calculator.git
+
+2. Navigate to the project directory:
+
+    ```bash
+    cd dart-calculator
+    ```
+
+3. Run the Dart program:
+
+    ```bash
+    dart calculator.dart
+    ```
+
+4. Follow the prompts to enter the first number, second number, and the desired operation.
+
+### Input
+    The program supports decimal input for more accurate calculations.
+
+### Error Handling
+    Division by zero and modulus with zero are handled, and appropriate error messages are displayed.
+### Contributing
+    Feel free to contribute to the project by submitting pull requests or reporting issues. Your feedback is valuable!
---
+++ calculator.dart
@@ -0,0 +1,58 @@
+import 'dart:io';
+
+dynamic calculate(num1, num2, operator_sign) {
+  if (operator_sign == '+') {
+    var sum = num1 + num2;
+    print('Sum is: $sum');
+  } else if (operator_sign == '-') {
+    var difference = num1 - num2;
+    print('Difference is: $difference');
+  } else if (operator_sign == '*') {
+    var product = num1 * num2;
+    print('Product is: $product');
+  } else if (operator_sign == '/') {
+    if (num2 != 0) {
+      var quotient = num1 / num2;
+      print('Quotient is: $quotient');
+    } else {
+      print("Error: Cannot divide by zero!");
+    }
+  } else if (operator_sign == '%') {
+    if (num2 != 0) {
+      var modulus = num1 % num2;
+      print('Modulus is: $modulus');
+    } else {
+      print("Error: Cannot calculate modulus with zero!");
+    }
+  } else {
+    print("Invalid operator!!");
+  }
+}
+
+double first_digit_input() {
+  stdout.writeln("Enter the first digit: ");
+  var first_digit = double.parse(stdin.readLineSync()!);
+  print("Your first digit is $first_digit");
+  return first_digit;
+}
+
+double second_digit_input() {
+  stdout.writeln("Enter the second digit: ");
+  var second_digit = double.parse(stdin.readLineSync()!);
+  print("Your second digit is $second_digit");
+  return second_digit;
+}
+
+String operator_sign_input() {
+  stdout.writeln("Enter the operation you wish to apply: ");
+  String oper = stdin.readLineSync()!;
+  return oper;
+}
+
+void main() {
+  var one = first_digit_input();
+  var two = second_digit_input();
+  String operator = operator_sign_input();
+
+  calculate(one, two, operator);
+}
